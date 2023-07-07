d8ee3d93f2861834b9a840e869fa268c123e4b88
masquernya <127450910+masquernya@users.noreply.github.com>
2023-07-07 12:28:34 -0400

    - refactor help command - add encrypt-nacl/decrypt-nacl (for plain text encryption without using files)

--- main.go
+++ main.go
@@ -6,15 +6,48 @@ import (
 	"github.com/masquernya/go-encryption-program/encryption"
 	"github.com/masquernya/go-encryption-program/ferret"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
+
+var commands = map[string]struct {
+	Arguments   []string
+	Description string
+}{
+	"help": {
+		Arguments:   []string{},
+		Description: "print this help message",
+	},
+	"encrypt-file": {
+		Arguments:   []string{"<publickey>", "<filepath>"},
+		Description: "encrypt file with public key, saving to <filepath>.enc",
+	},
+	"decrypt-file": {
+		Arguments:   []string{"<publickey>", "<filepath>"},
+		Description: "decrypt file, saving to <filepath>.dec. private key is read from the PRIVATE_KEY environmental variable.",
+	},
+	"genkey": {
+		Arguments:   []string{},
+		Description: "generate public and private key, then print it to the terminal",
+	},
+	"decrypt-nacl": {
+		Arguments:   []string{"<publickey>", "<message>"},
+		Description: "decrypt anonymous nacl box message (Base64 encoded) and print it to the terminal. private key is read from the PRIVATE_KEY environmental variable.",
+	},
+	"encrypt-nacl": {
+		Arguments:   []string{"<publickey>", "<message>"},
+		Description: "encrypt message with public key and print it to the terminal (Base64 encoded)",
+	},
+}
 
 func printHelp() {
 	fmt.Println("OwO1 Encryption Standard. Essentially NaCL box with chunk support.")
 	fmt.Println("Commands:")
-	fmt.Println("  help                           print help command")
-	fmt.Println("  encrypt <publickey> <filepath> encrypt file with public key, saving to <filepath>.enc")
-	fmt.Println("  decrypt <publickey> <filepath> decrypt file, saving to <filepath>.dec. private key is read from the PRIVATE_KEY environmental variable.")
-	fmt.Println("  generatekey                    generate public and private key, then print it to the terminal")
+	for cmd, data := range commands {
+		fmt.Print("\n")
+		fmt.Println("" + cmd + " " + strings.Join(data.Arguments, " "))
+		fmt.Println("    " + data.Description)
+	}
 	os.Exit(0)
 }
 
@@ -23,7 +56,7 @@ func main() {
 		printHelp()
 	}
 
-	if os.Args[1] == "generatekey" {
+	if os.Args[1] == "genkey" {
 		publicKey, privateKey, err := encryption.GenerateKeys()
 		if err != nil {
 			fmt.Println(err)
@@ -34,7 +67,7 @@ func main() {
 		fmt.Println("Private Key (Base64):")
 		fmt.Println(base64.StdEncoding.EncodeToString(privateKey))
 		os.Exit(0)
-	} else if os.Args[1] == "encrypt" {
+	} else if os.Args[1] == "encrypt-file" {
 		if len(os.Args) < 4 {
 			printHelp()
 		}
@@ -49,7 +82,7 @@ func main() {
 			panic(err)
 		}
 		fmt.Println("File encrypted and saved to " + outFilePath)
-	} else if os.Args[1] == "decrypt" {
+	} else if os.Args[1] == "decrypt-file" {
 		if len(os.Args) < 4 {
 			printHelp()
 		}
@@ -75,6 +108,59 @@ func main() {
 			panic(err)
 		}
 		fmt.Println("File decrypted and saved to " + outFilePath)
+	} else if os.Args[1] == "encrypt-nacl" {
+		if len(os.Args) < 3 {
+			printHelp()
+		}
+		publicKey, err := base64.StdEncoding.DecodeString(os.Args[2])
+		if err != nil {
+			panic(err)
+		}
+		message := os.Args[3]
+
+		encrypted, err := encryption.Encrypt(publicKey, []byte(message))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Encrypted message (Base64):")
+		fmt.Println(base64.StdEncoding.EncodeToString(encrypted))
+
+	} else if os.Args[1] == "decrypt-nacl" {
+		if len(os.Args) < 4 {
+			printHelp()
+		}
+		publicKey, err := base64.StdEncoding.DecodeString(os.Args[2])
+		if err != nil {
+			panic(err)
+		}
+		privateKeyStr, privateKeyExists := os.LookupEnv("PRIVATE_KEY")
+		if !privateKeyExists {
+			fmt.Println("Environment variable PRIVATE_KEY not found")
+			os.Exit(1)
+		}
+		privateKey, err := base64.StdEncoding.DecodeString(privateKeyStr)
+		if err != nil {
+			panic(err)
+		}
+
+		message, err := base64.StdEncoding.DecodeString(os.Args[3])
+		if err != nil {
+			panic(err)
+		}
+
+		decrypted, err := encryption.Decrypt(publicKey, privateKey, message)
+		if err != nil {
+			panic(err)
+		}
+
+		ok := utf8.Valid(decrypted)
+		if !ok {
+			fmt.Println("Message (Base64):")
+			fmt.Println(base64.StdEncoding.EncodeToString(decrypted))
+			os.Exit(0)
+		}
+		fmt.Println("Message:")
+		fmt.Println(string(decrypted))
 	} else {
 		printHelp()
 	}
