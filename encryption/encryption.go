ed4156ad8d9f5b82cf37d11ca4bf7fbbe26a215b
masquernya <127450910+masquernya@users.noreply.github.com>
2023-07-07 12:28:10 -0400

    add wrappers around box.SealAnon/box.OpenAnon

--- encryption/encryption.go
+++ encryption/encryption.go
@@ -2,6 +2,7 @@ package encryption
 
 import (
 	crypto_ran "crypto/rand"
+	"errors"
 	"golang.org/x/crypto/nacl/box"
 )
 
@@ -18,3 +19,21 @@ func GenerateKeys() ([]byte, []byte, error) {
 }
 
 const defaultBufferSize int = 1024 * 16 // 16kb
+
+// Encrypt encrypts the plainText using publicKey and returns the encrypted text.
+func Encrypt(publicKey []byte, plainText []byte) ([]byte, error) {
+	data, err := box.SealAnonymous(nil, plainText, (*[32]byte)(publicKey), crypto_ran.Reader)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// Decrypt decrypts the encryptedData using privateKey and returns the plain text.
+func Decrypt(publicKey []byte, privateKey []byte, encryptedData []byte) ([]byte, error) {
+	data, ok := box.OpenAnonymous(nil, encryptedData, (*[32]byte)(publicKey), (*[32]byte)(privateKey))
+	if !ok {
+		return nil, errors.New("decryption failed")
+	}
+	return data, nil
+}
